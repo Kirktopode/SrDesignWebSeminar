114957ef9f6cc58a2a36fe30afff711966211aab
Shizcow <pohl.devin@gmail.com>
2020-09-06 13:05:55 -0600

    Finished presentation, small formatting improvements that will help me teach

---
+++ widgets/button-tabs/script.js
@@ -0,0 +1,15 @@
+activeTab = 1
+
+function openTab(num) {
+    oldTab = document.getElementById("tab-" + activeTab)
+    newTab = document.getElementById("tab-" + num)
+    oldPage = document.getElementById("page-" + activeTab)
+    newPage = document.getElementById("page-" + num)
+    
+    oldTab.className = ""
+    oldPage.className = "page"
+    newTab.className = "active"
+    newPage.className = "page active"
+    
+    activeTab = num
+}
